bdb60f4f44cff81d4ed2974794007516b5147f1c
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-09-09 23:51:43 -0300

    Aprendendo sobre o framework Express

---
+++ nodejs/aula11_serverSimples/server.js
@@ -0,0 +1,28 @@
+const http = require("node:http")
+
+const server = http.createServer((request, response) => {
+    const path = request.url
+    switch (path){
+        case '/':
+            response.writeHead(200)
+            response.write("VocÊ está na página inicial")
+            break
+        case '/artigos':
+            response.writeHead(200)
+            response.write("VocÊ está na página de artigos")
+            break
+        default:
+            response.writeHead(404)
+            response.write("Not found")
+            break
+    }
+
+    response.end()
+})
+
+const PORT = 3000
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`)
+})
+
