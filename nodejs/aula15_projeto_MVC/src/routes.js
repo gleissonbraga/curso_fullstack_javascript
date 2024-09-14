4870032d03895fe00e110ce09f42dc42c84abde5
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-09-14 12:09:39 -0300

    Finalizando projeto guiado em padrão MVC

--- nodejs/aula15_projeto_MVC/src/routes.js
+++ nodejs/aula15_projeto_MVC/src/routes.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const postController = require('./controllers/postController')
+const adminController = require('./controllers/adminController')
 
 const router = express.Router()
 
+// Rotas do blog
 router.get('/', postController.index)
 // Pegando o id enviado via url do user
 router.get('/posts/:id', postController.show)
+
+// Rotas do Admin
+router.get('/admin', adminController.index)
+
+router.get('/admin/create', adminController.create)
+router.post('/admin/create', adminController.save)
+
+router.get('/admin/edit/:id', adminController.edit)
+router.post('/admin/update/:id', adminController.update)
+
+router.post('/admin/delete/:id', adminController.delete)
 
 module.exports = router
