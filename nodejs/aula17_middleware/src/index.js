c9ab09299e7a8ccf62199419778a131d2dde84fc
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-09-18 23:28:18 -0300

    Aprendendo middlewares

---
+++ nodejs/aula17_middleware/src/index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const middlewareC = require('./middleware/middlewar-c')
+
+// utilizando middleware da forma correta
+// baixando pacote especifico (multer "trabalha com upload de arquivos")
+
+
+
+const app = express()
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server: http://localhost${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+// **********************************************
+// usando middleware de forma simples
+
+
+// app.use(middlewareC)
+
+// // Metodo padrão para usar middleware
+// // app.use()
+
+// // primeiro modo de criar uma middleware
+// app.use(function (req, res, next) {
+//     console.log('Executando middleware A')
+//     req.middlewareA = 'OK!'
+//     next()
+// })
+
+// // segundo modo sem app.use
+// function middlewareB(req, res, next) {
+//     console.log('Executando middleware B')
+//     req.middlewareB = 'OK!'
+//     next()
+// }
+
+// app.get('/testeA', (req, res) => {
+//     console.log({ a: req.middlewareA, b: req.middlewareB })
+//     throw new Error('Algo deu muito errado')
+//     res.end()
+// })
+
+// app.get('/testeB', middlewareB, (req, res) => {
+//     console.log({ a: req.middlewareA, b: req.middlewareB })
+//     res.end()
+// })
+
+// app.use(function (err, req, res, next) {
+//     if(err) {
+//         console.log(err.message)
+//         res.status(400)
+//         res.json({message: err.message})
+//     } else {
+//         next()
+//     }
+// })
