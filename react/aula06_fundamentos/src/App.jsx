4887a50929fbb3dce0de111ad9621a55153474be
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-07 22:55:22 -0300

    Aprendendo os fundaemntos do React

---
+++ react/aula06_fundamentos/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+  const tech = "JavaScript"
+  
+  return (
+    <div>
+      <h1> {tech} is awesome </h1>
+    </div>
+  )
+}
---
+++ react/desafio01_Pagina_HTML/src/App.jsx
@@ -0,0 +1,29 @@
+export default function App(){
+  return (
+    <div className="app">
+      <img src="/react.png" alt="Logo do React" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas</p>
+      <div>
+        <button>Aprenda React</button>
+        <button>Referência da APU</button>
+      </div>
+      <hr />
+      <h2>Crie interfaces de usuário de componentes</h2>
+      <p>React permite que você construa interfaces de usuário a partir de pedaos individuais chamados componentes</p>
+      <hr />
+      <h2>Escreva componentes com código de marcação</h2>
+      <p>Componente React são funções JAvaScript. A sintaxe de marcação é chamada de JSX. É uma extensão de sintaxe JavaScript popularizada pelo React</p>
+      <hr />
+      <h2>Próximos passos</h2>
+      <ul>
+        <li>Uso de dados dinâmicos</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componetes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  )
+}
