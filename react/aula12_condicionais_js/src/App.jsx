0c748ac75d0aba2a8ccbb436750f7e26c09209c5
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-11-03 22:15:42 -0300

    Desafio feed de comentarios

--- react/aula12_condicionais_js/src/App.jsx
+++ react/aula12_condicionais_js/src/App.jsx
@@ -4,9 +4,12 @@ import Input from "./components/input"
 function App() {
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
-  const [passwordSize, setPasswordSize] = useState(12)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
 
   
+  const passwordSize = showInput ? customSize : 8
+
   function generate(){
     const characters = "1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
     let newPassword = ""
@@ -28,9 +31,33 @@ function App() {
    <div className="app">
       <h1>Gerador de senhas</h1>
       <div>
-        <label htmlFor="passwordSize">Tamanho:</label>
-        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize}/>
+        <label htmlFor="showInput">Customizar tamanho:</label>
+        <input 
+            type="checkbox"
+            id="showInput"
+            value={showInput}
+            onChange={() => setShowInput(currentState => !currentState)}
+        
+        />
       </div>
+
+      {/* Operador ternário */}
+      {/* {showInput ? (
+        <div>
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <Input passwordSize={customSize} setPasswordSize={setCustomSize}/>
+        </div>
+
+      ) : null} */}
+
+      {/* operação de curto circuito */}
+      {showInput && (
+        <div>
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <Input passwordSize={customSize} setPasswordSize={setCustomSize}/>
+        </div>
+
+      )}
       <button onClick={generate}>Gerar senha de {passwordSize} caracteres</button>
       <button onClick={copyToClipboard}>{copyText}</button>
       {/* <button onClick={() => setPasswordSize(99)}>Mudar estado</button> */}
