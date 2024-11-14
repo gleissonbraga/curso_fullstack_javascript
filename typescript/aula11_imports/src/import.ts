f159ede23fb33b4acffa65e772972d643420def9
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-11-13 23:52:03 -0300

    Entrando em generics no typescript

---
+++ typescript/aula11_imports/src/import.ts
@@ -0,0 +1,15 @@
+import { Spaceship } from "./export";
+import * as _ from 'lodash'
+
+interface BattleSpaceship extends Spaceship {
+    weapons: number
+}
+
+let xwing: BattleSpaceship = {
+    name: "X-wing",
+    pilot: "Luke Skywalker",
+    speed: 50,
+    weapons: 4
+}
+
+console.log(_.camelCase(xwing.pilot))
