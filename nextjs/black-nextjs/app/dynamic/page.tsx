deb94dde2089da1ba55162b2827c133946228ec7
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-01-03 19:55:11 -0300

    Realizando atualizações do next

---
+++ nextjs/black-nextjs/app/dynamic/page.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from "next"
+import { Col, Container, Row } from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Dynamic: NextPage = () => {
+  return (
+    <Container tag="main">
+      <h1 className="my-5">
+        Como funcionam as renderizações do Next.js
+      </h1>
+
+      <Row>
+        <Col>
+          <h3>
+            Gerado no servidor:
+          </h3>
+        </Col>
+
+        <Col>
+          <h3>
+            Gerado no cliente:
+          </h3>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Dynamic
--- nextjs/black-nextjs/app/page.tsx
+++ nextjs/black-nextjs/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Head from 'next/head'
 
 export default function Home() {
